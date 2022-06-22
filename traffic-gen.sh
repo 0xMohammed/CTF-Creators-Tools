e3c39e098ed61f6bd95c5ff37358b5fef03c6658
Mohammed Hassan <51376376+0xMohammed@users.noreply.github.com>
2022-06-22 22:17:48 +0200

    Update traffic-gen.sh

--- traffic-gen.sh
+++ traffic-gen.sh
@@ -4,6 +4,6 @@ do
 	do
 		IFS='.' read -a payload <<< "$src"
 		sleep $(shuf -i 0-15 -n 1)
-		nping -S $src --data-length ${payload[0]} --tcp $dst
+		nping -S $src --data-length ${payload[0]} --tcp -p$(shuf -i 1-1024 -n 1) $dst
 	done
 done
