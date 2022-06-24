402bd16667c5b04a6fad8b3be9dd4b0ce66d292d
Mohammed Hassan <51376376+0xMohammed@users.noreply.github.com>
2022-06-24 17:41:00 +0200

    Update traffic-gen.sh

--- traffic-gen.sh
+++ traffic-gen.sh
@@ -1,4 +1,4 @@
 for dst in $(for i in {1..50}; do cat victimlist.txt; done | shuf)
 do
-	sudo nping -S random --data-length random --tcp -p$(shuf -i 1-1024 -n 1) --source-mac random --ack random --win random --tos random --id random --mtu random --rate random --delay $(shuf -i 0-10 -n 1) -c random --dest-ip $dst
+	sudo nping -S random --data-length random --tcp -p$(shuf -i 1-1024 -n 1) --source-mac random --ack random --win random --tos random --id random --mtu random --rate random --delay $(shuf -i 0-10 -n 1) -c random --dest-ip --source-mac random $dst
 done
